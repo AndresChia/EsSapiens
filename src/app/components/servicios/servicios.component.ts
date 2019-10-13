5e04badb24024058ed0b80d95e14ba4901865028
Andres Chia <andres_chia11@hotmail.com>
2019-10-13 16:47:09 -0500

    COMMIT: cambios en serviios

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -7,18 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiciosComponent implements OnInit {
 
-  servicioActual = 0;
+  servicioActual = 3;
 
   informacionServicios = [{
-    nombre: ""
+    titulo: "Soporte",
+    nombre: "Todo lo que quieres y como lo quieres",
+    informacion: "Soporte 24/7 para garantizar que tu servicios esten siempre bajo tus condiciones, servicios de postventa y preventa.",
+    img: "../../../assets/images/icon4.png"
   }, {
-    nombre: ""
+    titulo: "Disponibilidad",
+    nombre: "Siempre a la mano de tu usuario",
+    informacion: "Tus servicios siempre estaran disponibles, los servidores donde se aloja la información siempre estaran encendidos y pendientes de tus peticiones.",
+    img: "../../../assets/images/icon2.png"
+  }, {
+    titulo: "Usabilidad",
+    nombre: "Echo bajo tus exigencias",
+    informacion: "Se realizara bajo tus peticiones y especificaciones para garantizar que tus clientes se sientan agradados con el uso.",
+    img: "../../../assets/images/icon1.png"
+  }, {
+    titulo: "Seguridad",
+    nombre: "Sin fugas de datos",
+    informacion: "Tu información estara bajo proteccion siempre y los protocolos de intercambio estaran cifrados.",
+    img: "../../../assets/images/icon3.png"
   }
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  hover(item, index) {
+    this.servicioActual = index;
   }
 
 }
